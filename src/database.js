de273047a0d8b6af1551cbe9ee12d07f4dfeaa43
Adrián Varela <adrianvarela13.13@gmail.com>
2024-04-19 23:40:27 +0200

    Update database.js

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 (async () => {
   try {
     const db = await connect(
-      "mongodb+srv://adrianvarela:hccq3ksSiblbUcWZ@cluster0.5n6rbys.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://adrianvarela:p5usDWlCJL1p1lRF@cluster0.8fp7j8e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log("DB connected to: ", db.connection.name);
   } catch (error) {
